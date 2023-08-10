d2df06827ce8e21e2dfbfa8859d4f9c7e968583b
root <victoriamuthoni2@gmail.com>
2023-08-10 12:15:05 -0700

    Changes

--- 3-infinite_add.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-if __name__ == "__main__":
-    import sys
-    total = 0
-    if (len(sys.argv) > 1):
-        for i in range(1, len(sys.argv)):
-            total += (int(sys.argv[i]))
-    print("{:d}".format(total))
